b8a22f6fe5c2bc50009899c6d84b3d65b7091a3e
EtowTheOffieCat <mabonp@gmail.com>
2019-04-05 13:11:07 +0200

    Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.5f;
+    public float rotationSpeed = 100f;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0, 0, - speed * Time.deltaTime)); 
+        }
+
+        else if  (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(new Vector3(0, - rotationSpeed * Time.deltaTime, 0));
+        }
+    }
+}
